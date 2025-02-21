7a37e5e2790ad7e6f586eba35e18b1679505585c
Salman-Hussain21 <isalmanhussain21@gmail.com>
2025-02-21 16:46:17 +0500

    comiting aasync , await, delay task

--- bin/function_practise.dart
+++ bin/function_practise.dart
@@ -1,30 +1,75 @@
-void main(){
-dosomething1();
+// void main(){
+// dosomething1();
+//
+// var result = sum(10, 20);
+// print(result);
+// result = sum2(10, 30);
+//
+// var msg = getMessage(message: "NO CAP");
+// print(msg);
+//
+// dosomething3(msg: "Bobzy The King", times: 5);
+// }
+//
+// void dosomething1(){
+//   print("something is being done");
+// }
+//
+// int sum(int num1 , int num2){
+//   return num1+num2;
+// }
+//
+// int sum2(int num1, int num2)=> num1+num2;
+//
+// String getMessage({required String message})=> "The message is $message";
+//
+// void dosomething3({required String msg, int times=1}){
+//   for(int cntr = 0; cntr<times; cntr++){
+//     print(msg);
+//   }
+// }
 
-var result = sum(10, 20);
-print(result);
-result = sum2(10, 30);
 
-var msg = getMessage(message: "NO CAP");
-print(msg);
+void main() async{
 
-dosomething3(msg: "Bobzy The King", times: 5);
+
+//   int value1 =  await Future.delayed(Duration(seconds: 10),()=>1000);
+//   print("The value is $value1");
+
+//   int value2 = await Future.delayed(Duration(seconds: 2),()=>10);
+
+//   print("the value is $value2");
+
+//   print("Have a nice day");
+
+  //Future value = getValue();
+
+  //int value = await getValue();
+
+  getSecond().then((v){
+    print("the value is $v");
+  });
+
+
+  getFirst().then((v){
+    print("the value is $v");
+  });
+  print("have a nice day");
+
+
 }
 
-void dosomething1(){
-  print("something is being done");
+
+Future<int> getSecond()async{
+  await Future.delayed(Duration(seconds:10));
+  return Future.value(100);
+
 }
 
-int sum(int num1 , int num2){
-  return num1+num2;
-}
 
-int sum2(int num1, int num2)=> num1+num2;
 
-String getMessage({required String message})=> "The message is $message";
+Future<int> getFirst()async{
+  await Future.delayed(Duration(seconds:5));
+  return Future.value(10);
 
-void dosomething3({required String msg, int times=1}){
-  for(int cntr = 0; cntr<times; cntr++){
-    print(msg);
-  }
 }
