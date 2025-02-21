436f419aea469c5f6234c0e35d4c4cba71316597
Salman-Hussain21 <isalmanhussain21@gmail.com>
2025-02-21 15:53:57 +0500

    comitting the task

---
+++ bin/function_practise.dart
@@ -0,0 +1,30 @@
+void main(){
+dosomething1();
+
+var result = sum(10, 20);
+print(result);
+result = sum2(10, 30);
+
+var msg = getMessage(message: "NO CAP");
+print(msg);
+
+dosomething3(msg: "Bobzy The King", times: 5);
+}
+
+void dosomething1(){
+  print("something is being done");
+}
+
+int sum(int num1 , int num2){
+  return num1+num2;
+}
+
+int sum2(int num1, int num2)=> num1+num2;
+
+String getMessage({required String message})=> "The message is $message";
+
+void dosomething3({required String msg, int times=1}){
+  for(int cntr = 0; cntr<times; cntr++){
+    print(msg);
+  }
+}
